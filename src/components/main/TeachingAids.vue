6184f5c8ad18171061c2eb3d626e29e704c30711
ollehints <olle.hints@ttu.ee>
2019-03-17 13:06:33 +0200

    show teaching modules from API, update footer logos, closes #66 and #67

--- src/components/main/TeachingAids.vue
+++ src/components/main/TeachingAids.vue
@@ -1,35 +1,33 @@
 <template>
-  <div v-if="isAuthenticated">
-
+  <div class="main">
     <div class="row">
       <div class="col">
-        <h2>Teaching Aids</h2>
+        <h2>Teaching materials</h2>
+      </div>
+    </div>
+    
+    <div v-if="results.length > 0" v-for="module in results" class="row mt-3">
+      <div class="col">
+	  	<div class="module-details">
+	  		<h3>{{ module.title }}</h3>
+	        <p><strong>Authors</strong>: {{ module.author }}, {{ module.institution__name }}</p>
+	        <p><strong>Description</strong>: {{ module.description }}</p>
+	        <p><strong>Downloads</strong>: <a href="javascript:void(0)" @click="openInNewWindow({url: module.files__url})">{{ module.files__title }}</a></p>
+		</div>
       </div>
     </div>
 
-    <div class="row mt-3">
-      <div class="col">
-        <h3>Teaching materials for Masters students</h3>
-        <ul>
-          <li><a href="#/teaching_aids">Exercise 1</a></li>
-          <li><a href="#/teaching_aids">Exercise 2</a></li>
-          <li><a href="#/teaching_aids">Exercise 3</a></li>
-          <li><a href="#/teaching_aids">Exercise 4</a></li>
-          <li><a href="#/teaching_aids">Exercise 5</a></li>
-        </ul>
-      </div>
-    </div>
-  </div>
-  <div v-else>
-    <div class="row">
-      <div class="col">
-        <p class="text-center error-text">
-          <b>Teaching materials are available for registered users.</b>
-          <br>
-          For getting an account please contact project manager: <a href="mailto:info@eurocore.rocks" target="_top">info@eurocore.rocks</a>
-        </p>
-      </div>
-    </div>
+	  <div v-if="!isAuthenticated">
+	    <div class="row">
+	      <div class="col">
+	        <p class="text-center error-text">
+	          <b>Please note that some teaching materials are available for registered users only.</b>
+	          <br>
+	          For getting an account please contact project partners.
+	        </p>
+	      </div>
+	    </div>
+	  </div>
   </div>
 </template>
 
@@ -38,7 +36,9 @@
     name: "teaching-aids",
     data() {
       return {
-        isAuthenticated: false
+        isAuthenticated: false,
+        API_URL: 'https://api.eurocore.rocks/teaching_module/',
+        results: null,
       }
     },
     metaInfo: {
@@ -48,14 +48,62 @@
       if (this.$session.exists() && this.$session.get('userData') != null) {
         this.isAuthenticated = true;
       }
+      this.getModules();
     },
+    methods: {
+        getModules() {
+          this.isSearching = true
+          this.$http.get(this.API_URL, {
+            before(request) {
+
+              // abort previous request, if exists
+              if (this.previousRequest) {
+                this.previousRequest.abort();
+              }
+
+              // set previous request on Vue instance
+              this.previousRequest = request;
+            },
+            params: {format: 'json'}
+          }).then(response => {
+            this.isSearching = false
+            console.log(response);
+            if (response.status === 200) {
+              this.results = response.body.results;
+              console.log('results:' + JSON.stringify(this.results));
+            }
+          }, errResponse => {
+            this.isSearching = false
+            console.log('ERROR: ' + JSON.stringify(errResponse));
+          });
+        },
+        openInNewWindow(params) {
+            if (typeof (params.width) === 'undefined') {
+              params.width = 800;
+            }
+            window.open('https://eurocore.rocks' + params.url,'', 'width=' + params.width + ', height=750');
+        },
+    }
   }
 </script>
 
 <style scoped>
   .error-text {
-    font-size: 1.5rem;
+    font-size: 1.2rem;
     margin: 10% auto;
+    border: 1px solid pink;
+    display: block;
+  	padding: 10px;
   }
-
+.module-details {
+	margin: 0 0 0 3rem;
+}
+.module-details p {
+	margin: 0 0 0 3rem;
+    padding: 0;
+	line-height: 2rem;
+}
+.main{
+	padding: 0 0 10% 0;
+}
 </style>
--- src/components/main/partial/AppFooter.vue
+++ src/components/main/partial/AppFooter.vue
@@ -2,11 +2,14 @@
   <footer class="footer">
     <div>
       <a target="_blank" href="https://eitrawmaterials.eu" title="EIT RawMaterials">
-        <img id="eitrawLogo" src="https://eurocore.rocks/images/EITRM_flag_logo_web.png" alt="Eitraw logo"/>
+        <img id="eitrawLogo" src="https://eurocore.rocks/images/RM-Academy-Logo_300px.png" alt="Eitraw logo"/>
+      </a>
+      <a target="_blank" href="https://eitrawmaterials.eu" title="EU">
+        <img id="euLogo" src="https://eurocore.rocks/images/EU-Flag-2020_100px.png" alt="EU"/>
       </a>
     </div>
 
-    <div><font-awesome-icon :icon="icon" /> 2017-2018. EIT RawMaterials project EUROCORE: European Core Sample Collection for Master Training.</div>
+    <div><font-awesome-icon :icon="icon" /> 2017-2019. EIT RawMaterials project EUROCORE: European Core Sample Collection for Master Training.</div>
 
     <div>
       <a target="_blank" href="http://ttu.ee/geoloogia-instituut">Developed and hosted by the Department of Geology, Tallinn University of Technology</a> and <a target="_blank" href="http://natarc.ut.ee">NATARC</a>.
@@ -49,7 +52,10 @@
     height: 50px;
     margin: 10px;
   }
-
+  #euLogo {
+	    height: 50px;
+	    margin: 10px;
+	  }
   /* Footer logo is 40px for small mobile screens */
   @media (max-width: 401px) {
     #eitrawLogo {
