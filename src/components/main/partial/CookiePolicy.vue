ef6d331c18c0433f0b190368a2fe45608a5dd988
Enar <enmust@ttu.ee>
2019-03-25 22:45:51 +0200

    #68, design changes

--- src/components/main/partial/CookiePolicy.vue
+++ src/components/main/partial/CookiePolicy.vue
@@ -2,35 +2,35 @@
   <div class="cookie-policy" v-if="showCookiePolicy">
 
     <div class="cookie-ribbon">
-      <div class="col-lg-12">
+      <div class="col-lg-12 text-center">
         <span>
-          This site uses cookies. By continuing to browse the site, you are agreeing to our use of cookies. Read our
+          This site uses cookies. By continuing to browse the site, you are agreeing to our use of cookies.
         </span>
 
-        <span>
-          <a v-b-modal.cookie-policy>conditions of use.</a>
-        </span>
+        <!--<span>-->
+          <!--Read our -->
+          <!--<a>conditions of use.</a>-->
+        <!--</span>-->
 
         <span class="pl-2">
           <b-button variant="primary" @click="agreeToCookiePolicy">Agree</b-button>
         </span>
-
-        <span class="pb-3" style=" position: fixed;bottom: 0;right:0;">
-          <button type="button" class="btn btn-xs btn-link" style="color:white;font-size: small"
-                  aria-pressed="true" area-label="close cookie" title="close cookie">
-            <font-awesome-icon :icon="icon"/>
-          </button>
-        </span>
       </div>
+
+      <span class="exit-button" @click="showCookiePolicy = false">
+          <a title="close cookie policy">
+            <font-awesome-icon :icon="icon"/>
+          </a>
+        </span>
     </div>
 
     <!-- MODAL -->
-    <b-modal id="cookie-policy" centered scrollable title="Terms of Service, Cookie policy, Privacy Policy">
-      <p class="my-4" v-for="i in 20" :key="i">
-        Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis
-        in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-      </p>
-    </b-modal>
+    <!--<b-modal id="cookie-policy" centered scrollable title="Terms of Service, Cookie policy, Privacy Policy">-->
+      <!--<p class="my-4" v-for="i in 20" :key="i">-->
+        <!--Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis-->
+        <!--in, egestas eget quam. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.-->
+      <!--</p>-->
+    <!--</b-modal>-->
 
   </div>
 </template>
@@ -58,14 +58,15 @@
       icon() { return faTimes }
     },
     created() {
-      fetchPage(67).then((response) => {this.cookiePolicy = response.results[0]});
-      fetchPage(68).then((response) => {this.privacyPolicy = response.results[0]});
-      fetchPage(69).then((response) => {this.termsOfService = response.results[0]});
-      // const eurocoreCookiePolicy = this.$localStorage('eurocore_cookie_policy')
-      // console.log(eurocoreCookiePolicy)
-      this.showCookiePolicy = true
-
-
+      const userAgreedToCookiePolicy = this.$localStorage.get('eurocore_cookie_policy')
+      if (userAgreedToCookiePolicy) {
+        this.showCookiePolicy = false
+        // fetchPage(67).then((response) => {this.cookiePolicy = response.results[0]});
+        // fetchPage(68).then((response) => {this.privacyPolicy = response.results[0]});
+        // fetchPage(69).then((response) => {this.termsOfService = response.results[0]});
+      } else {
+        this.showCookiePolicy = true
+      }
     },
     methods: {
       agreeToCookiePolicy() {
@@ -78,7 +79,7 @@
 
 <style scoped>
   .cookie-ribbon {
-    background-color: #f8f9fa!important;
+    background-color: #ffffff;
     overflow: hidden;
     position: fixed;
     right: 0;
@@ -87,5 +88,18 @@
     padding: 8px 16px;
     box-shadow: 0 4px 10px #000;
     z-index: 1; /* Maybe need to make it even more higher */
+  }
+
+  .exit-button {
+    position: absolute;
+    top: 0;
+    right: 0;
+    padding: 0.5em 1em;
+    color: #007bff;
+    font-size: smaller;
+  }
+
+  .exit-button:hover {
+    cursor: pointer;
   }
 </style>
